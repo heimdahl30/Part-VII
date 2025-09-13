629df13748071385e982ee725af8bdbccce808f5
heimdahl30 <67910058+heimdahl30@users.noreply.github.com>
2025-09-13 19:22:46 +0530

    Delete Bloglist_frontend directory

--- Bloglist_frontend/src/services/user.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "/api/users";
-
-const getUsers = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
-};
-
-export default { getUsers };
